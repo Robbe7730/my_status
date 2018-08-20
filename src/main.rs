e87e5d7c5adaa3fcea2568757b99af11ecade1f8
Robbe Van Herck <robbevanherck1@gmail.com>
2018-08-20 19:14:10 +0200

    Added currently playing song

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ struct Header {
 #[derive(Serialize, Deserialize, Default, Clone)]
 struct Status {
     full_text: String,
-    
+
     #[serde(skip_serializing_if = "Option::is_none")]
     short_text: Option<String>,
     
@@ -136,7 +136,7 @@ fn time() -> Option<Status> {
 fn date() -> Option<Status> {
     let now: DateTime<Local> = chrono::Local::now();
     return Some(Status {
-    full_text: now.format("%a %e %b %Y").to_string(),
+        full_text: now.format("%a %e %b %Y").to_string(),
         ..Default::default()
     })
 }
@@ -199,6 +199,43 @@ fn volume() -> Option<Status> {
     });
 }
 
+fn playing() -> Option<Status> {
+    let status = Command::new("playerctl")
+                         .arg("status")
+                         .output()
+                         .expect("failed to execute process");
+    let status_str = String::from_utf8_lossy(&status.stdout).into_owned();
+    
+    let status_icon;
+
+    if status_str == "Playing\n" {
+        status_icon = "⏵";
+    } else if status_str == "Paused\n" {
+        status_icon = "⏸";
+    } else {
+        return None
+    }
+
+    let artist = Command::new("playerctl")
+                         .arg("metadata")
+                         .arg("artist")
+                         .output()
+                         .expect("failed to execute process");
+    let artist_str = String::from_utf8_lossy(&artist.stdout).into_owned();
+
+    let track = Command::new("playerctl")
+                         .arg("metadata")
+                         .arg("title")
+                         .output()
+                         .expect("failed to execute process");
+    let track_str = String::from_utf8_lossy(&track.stdout).into_owned();
+
+    return Some( Status {
+        full_text: format!("{} {} - {}", status_icon, track_str, artist_str),
+        ..Default::default()
+    });
+}
+
 fn header() -> String {
     let header = Header {
         version: 1,
@@ -209,6 +246,7 @@ fn header() -> String {
 
 fn status() -> String {
     let mut statusses: Vec<Option<Status>> = vec![
+        playing(),
         network(),
         volume(),
         battery(),
