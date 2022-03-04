470a305bb576f948c03152c58adec7ee5da3f9f7
Robbe Van Herck <robbe@robbevanherck.be>
2022-03-04 02:48:43 +0100

    Fixes to Battery module

--- src/modules/battery.rs
+++ src/modules/battery.rs
@@ -10,26 +10,20 @@ impl Module for BatteryModule {
     fn get_blocks(&self) -> Vec<StatusBlock> {
         match self.manager.batteries().unwrap().next() {
             Some(Ok(battery)) => {
+                let charging = battery.state() == State::Charging;
                 let percentage: f32 = (battery.state_of_charge() * 100.0).into();
-                let color = if percentage <= 25.0 && percentage > 15.0 {
+                let color = if !charging && (percentage <= 25.0 && percentage > 15.0) {
                     "#ff7f00"
                 } else {
                     "#ffffff"
                 };
-                let icon = match battery.state() {
-                    State::Full => "🔋",
-                    State::Charging => "☇",
-                    State::Unknown => "?",
-                    State::Empty => "🔋",
-                    State::Discharging => "🔋",
-                    State::__Nonexhaustive => "?"
-                };
+                let icon = if charging { "⚡" } else { "🔋" };
                 vec![
                     StatusBlock::new(
                         "battery",
                         &format!("{} {}%", icon, percentage.round())
                     ).with_color(color)
-                        .with_urgent(percentage <= 15.0)
+                        .with_urgent(percentage <= 15.0 && !charging)
                 ]
             },
             _ => vec![]
